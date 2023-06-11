6cae6bca25f0770b95f907a7323e009eee324e2b
Ignut <mister.strays@gmail.com>
2023-06-11 22:22:07 +0300

    Rework data base parser

--- kursovaya/MainForm.cs
+++ kursovaya/MainForm.cs
@@ -12,13 +12,15 @@ using systemoffurniture;
 using System.Windows.Forms.DataVisualization.Charting;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.Data.SqlClient;
-
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+using System.Windows.Threading;
 
 namespace systemoffurniture
 {
     public partial class MainForm : Form
     {
         private List<Recipe> _recipes = new List<Recipe>();
+        private BindingSource bindingSource;
 
         public MainForm()
         {
@@ -35,21 +37,19 @@ namespace systemoffurniture
             SQLiteConnection connection = new SQLiteConnection("Integrated Security = SSPI; Data Source = db.db");
             connection.Open();
             var command = connection.CreateCommand();
-            command.CommandText = @"SELECT ID, Name, Country, Difficult   FROM Furniture";
+            command.CommandText = @"SELECT ID, Name, Country, Difficult   FROM Recipes";
             using (var rd1 = command.ExecuteReader())
             {
 
                 while (rd1.Read())
                 {
-
                     _recipes.Add(new Recipe(rd1.GetInt32(0), rd1.GetString(1), rd1.GetString(2), rd1.GetInt32(3)));
-
                 }
             }
             connection.Close();
             connection.Open();
 
-            command.CommandText = @"SELECT Country, COUNT(*) AS Count FROM Furniture GROUP BY Country";
+            command.CommandText = @"SELECT Country, COUNT(*) AS Count FROM Recipes GROUP BY Country";
             using (var reader = command.ExecuteReader())
             {
                 while (reader.Read())
@@ -61,89 +61,52 @@ namespace systemoffurniture
                 }
             }
             connection.Close();
-            dataGridView1.DataSource = _recipes;
+
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = _recipes;
+            dataGridView1.DataSource = bindingSource;
 
             comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox1.Items.Add("Все товары категории Кухня");
-            comboBox1.Items.Add("Товары дешевле 1500");
-            comboBox1.Items.Add("Все товары категории Офис");
-            comboBox1.Items.Add("Все товары категории Ванная");
-            comboBox1.Items.Add("Все товары с именем Стол");
-            comboBox1.Items.Add("Количество товаров по категориям");
-            comboBox1.Items.Add("Товары, сгрупированные по категориям");
-            comboBox1.Items.Add("Товары, имя которых начинается на С");
-            comboBox1.Items.Add("Товары дороже 1500");
+            comboBox1.Items.Add("Все рецепты сложностью <3");
+            comboBox1.Items.Add("Все рецепты из России");
+            comboBox1.Items.Add("Все рецепты из Японии");
+            comboBox1.Items.Add("Все рецепты из Турции");
+            comboBox1.Items.Add("Все рецепты из Италии");
+            comboBox1.Items.Add("Рецепты сложнее 4");
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (comboBox1.SelectedIndex == 0)
             {
-                IEnumerable<Recipe> kitchen = _recipes.Where(n => n.Name == "Кухня");
+                IEnumerable<Recipe> kitchen = _recipes.Where(recipe => recipe.Difficult < 3);
                 this.dataGridView2.DataSource = kitchen.ToArray();
             }
             if (comboBox1.SelectedIndex == 1)
             {
-                IEnumerable<Recipe> numQuery4 = _recipes.Where(n => n.Difficult < 1500);
+                IEnumerable<Recipe> numQuery4 = _recipes.Where(recipe => recipe.Country == "Россия");
                 this.dataGridView2.DataSource = numQuery4.ToArray();
             }
 
             if (comboBox1.SelectedIndex == 2)
             {
-                IEnumerable<Recipe> kitchen = _recipes.Where(n => n.Name == "Офис");
+                IEnumerable<Recipe> kitchen = _recipes.Where(recipe => recipe.Country == "Япония");
                 this.dataGridView2.DataSource = kitchen.ToArray();
             }
 
             if (comboBox1.SelectedIndex == 3)
             {
-                IEnumerable<Recipe> kitchen = _recipes.Where(n => n.Name == "Ванная");
+                IEnumerable<Recipe> kitchen = _recipes.Where(recipe => recipe.Country == "Турция");
                 this.dataGridView2.DataSource = kitchen.ToArray();
             }
 
             if (comboBox1.SelectedIndex == 4)
             {
-                IEnumerable<Recipe> kitchen = _recipes.Where(n => n.Country == "Стол");
+                IEnumerable<Recipe> kitchen = _recipes.Where(recipe => recipe.Country == "Италия");
                 this.dataGridView2.DataSource = kitchen.ToArray();
             }
 
             if (comboBox1.SelectedIndex == 5)
-            {
-                var query = _recipes.GroupBy(pet => pet.Name, pet => pet.Country);
-
-                foreach (var petGroup in query)
-                {
-                    richTextBox1.AppendText("Категория  - " + petGroup.Key + " \n");
-
-                    foreach (string name in petGroup)
-                    {
-                        richTextBox1.AppendText(" " + name + "  \n");
-                    }
-                    richTextBox1.AppendText(" Колличество - " + _recipes.Where(pet => pet.Name == petGroup.Key).Count().ToString() + " \n");
-                    richTextBox1.AppendText("___________________________________ \n");
-                }
-            }
-
-            if (comboBox1.SelectedIndex == 6)
-            {
-                var query = _recipes.GroupBy(n => n.Name, n => n.Country);
-                foreach (var group in query)
-                {
-                    richTextBox1.AppendText(group.Key + "  \n");
-                    foreach (var item in group)
-                    {
-                        richTextBox1.AppendText("  " + item + "  \n");
-                    }
-                    richTextBox1.AppendText("--------------------------------- \n");
-                }
-            }
-
-            if (comboBox1.SelectedIndex == 7)
-            {
-                IEnumerable<Recipe> kitchen = _recipes.Where(n => n.Country.StartsWith("С"));
-                this.dataGridView2.DataSource = kitchen.ToArray();
-            }
-
-            if (comboBox1.SelectedIndex == 8)
             {
                 IEnumerable<Recipe> numQuery4 = _recipes.Where(n => n.Difficult > 1500);
                 this.dataGridView2.DataSource = numQuery4.ToArray();
@@ -151,19 +114,63 @@ namespace systemoffurniture
         }
 
         private void button1_Click(object sender, EventArgs e)
-
         {
-            SQLiteConnection connection = new SQLiteConnection("Integrated Security = SSPI; Data Source = db.db");
-            connection.Open();
-            SQLiteCommand command = new SQLiteCommand($"INSERT INTO [Furniture] (ID, Name, Contry, Difficult) VALUES (@ID, @Name, @Category, @Price)", connection);
-            command.Parameters.AddWithValue("ID", textBox1.Text);
-            command.Parameters.AddWithValue("Name", textBox2.Text);
-            command.Parameters.AddWithValue("Contry", textBox3.Text);
-            command.Parameters.AddWithValue("Difficult", textBox4.Text);
-            MessageBox.Show("Товар успешно добавлен", command.ExecuteNonQuery().ToString());
+            AddData();
+        }
+
+        private async void AddData()
+        {
+                chart1.Series["Series1"].Points.Clear();
+
+            await Task.Run(() =>
+            {
+                SQLiteConnection connection = new SQLiteConnection("Integrated Security = SSPI; Data Source = db.db");
+                connection.Open();
+                SQLiteCommand command = new SQLiteCommand($"INSERT INTO [Recipes] (ID, Name, Country, Difficult) VALUES (@ID, @Name, @Country, @Difficult)", connection);
+                command.Parameters.AddWithValue("ID", textBox1.Text);
+                command.Parameters.AddWithValue("Name", textBox2.Text);
+                command.Parameters.AddWithValue("Country", textBox3.Text);
+                command.Parameters.AddWithValue("Difficult", textBox4.Text);
+                MessageBox.Show("Товар успешно добавлен", command.ExecuteNonQuery().ToString());
+
+                connection.Close();
+                connection.Open();
+
+                command = connection.CreateCommand();
+                command.CommandText = @"SELECT ID, Name, Country, Difficult   FROM Recipes";
+                _recipes.Clear();
+                using (var rd1 = command.ExecuteReader())
+                {
+
+                    while (rd1.Read())
+                    {
+
+                        _recipes.Add(new Recipe(rd1.GetInt32(0), rd1.GetString(1), rd1.GetString(2), rd1.GetInt32(3)));
+
+                    }
+                }
+
+                connection.Close();
+                connection.Open();
+
+                command.CommandText = @"SELECT Country, COUNT(*) AS Count FROM Recipes GROUP BY Country";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string category = reader.GetString(0);
+                        int count = reader.GetInt32(1);
+                        chart1.Series[0].ChartType = SeriesChartType.Column;
+                        chart1.Series["Series1"].Points.AddXY(category, count);
+                    }
+                }
+                connection.Close();
+            });
+
+            bindingSource.ResetBindings(false);
 
         }
- 
+
     }
 
 }
